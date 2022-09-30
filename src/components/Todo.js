c8774eadd0d1ec11db20a0b97a3ad836b8f53c26
georgiev098 <Georgiev.Daniel@outlook.com>
2022-09-30 14:38:58 +0200

    Vid 14: handling click events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,15 @@
 const Todo = ({ title }) => {
+  const deleteHandler = () => {
+    console.log("Clicked!");
+  };
+
   return (
     <div className="card">
       <h2>{title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
