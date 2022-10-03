fbb1cfc1f5e96992bd51d7f75cc077d2ba68d9d0
georgiev098 <Georgiev.Daniel@outlook.com>
2022-10-03 12:35:18 +0200

    vid - 20 - add navbar and links

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMettup";
 import FavoritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
+
       <Switch>
         <Route path={"/"} exact>
           <AllMeetupsPage />
