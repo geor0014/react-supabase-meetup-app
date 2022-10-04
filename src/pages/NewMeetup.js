4aa3dc7344719e9c62d17705c210b6dee348c372
georgiev098 <Georgiev.Daniel@outlook.com>
2022-10-04 20:35:37 +0200

    vid - 29/30 - add supabase and enable inserting data into supabase in NewMeetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,30 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { createClient } from "@supabase/supabase-js";
+import { useHistory } from "react-router-dom";
 
 function NewMeetup() {
+  // history is a React hook that allows us to redirect the user to a different page
+  const history = useHistory();
+  // create a new client
+  const supabaseUrl = "https://itfqisbmsktbfvumjmdw.supabase.co";
+  const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
   // Supabase request
-  const addMeetupHandler = async (meetupData) => {};
+  const addMeetupHandler = async (meetupData) => {
+    try {
+      // insert the meetup data into the meetups table
+      const { data, error } = await supabase.from("meetups").insert(meetupData);
+      console.log("data", data);
+      // redirect the user to the AllMeetups page
+      history.replace("/");
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   return (
     <section>
