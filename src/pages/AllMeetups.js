fd753e0b112db33897cd7ebebb7472774a3113c0
georgiev098 <Georgiev.Daniel@outlook.com>
2022-10-04 21:01:17 +0200

    vid - 30/31 - introduce UseEffect hook and get all data from supabase in AllMeetups page

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,45 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  // create a new client
+  const supabaseUrl = "https://itfqisbmsktbfvumjmdw.supabase.co";
+  const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // get data from supabase
+    const fetchMeetups = async () => {
+      try {
+        const { data, error } = await supabase.from("meetups").select();
+        if (error) {
+          throw error;
+        }
+        setLoadedMeetups(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchMeetups();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
